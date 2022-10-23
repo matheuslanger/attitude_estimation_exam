1b1e7be67cef8db514edbd7d6edec47af910fb4e
Matheus Langer <matheus.araujo.langer@gmail.com>
2022-10-23 01:05:55 -0300

    Added capability to write a desired value to a file as well as making each line from the file a object from a class

--- main.cpp
+++ main.cpp
@@ -10,63 +10,23 @@ using namespace std;
 
 class SteeringData
 {
+
 public:
-    SteeringData(
-        string timestamp,
-        string acc_x,
-        string acc_y,
-        string acc_z)
-    {
-        Timestamp = timestamp;
-        Acc_x = acc_x;
-        Acc_y = acc_y;
-        Acc_z = acc_z;
-    }
+        string Timestamp;
+        string Acc_x;
+        string Acc_y;
+        string Acc_z;
 
-    void display()
-    {
-        cout << "Timestamp: " << Timestamp << endl;
-        cout << "Acc_x: " << Acc_x << endl;
-        cout << "Acc_y: " << Acc_y << endl;
-        cout << "Acc_z: " << Acc_z << endl;
-        cout << endl;
-    }
-
-    string Timestamp;
-    string Acc_x;
-    string Acc_y;
-    string Acc_z;
 };
-
-void displayData(vector<SteeringData> &data)
-{
-
-    for (auto steer : data)
-    {
-        steer.display();
-    }
-}
-
-SteeringData dataParser(string line, SteeringData steer)
-{
-
-    stringstream inputString(line);
-    line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
-    getline(inputString, steer.Timestamp, ';');
-    getline(inputString, steer.Acc_x, ';');
-    getline(inputString, steer.Acc_y, ';');
-    getline(inputString, steer.Acc_z, ';');
-    return steer;
-}
 
 int main()
 {
     ifstream file;
 
-    int line_number;
-    line_number = 50000;
+    ofstream writeFile;
 
     file.open("attitude_exam.log");
+    writeFile.open("processed_data.log", ofstream::out | ofstream::trunc);
 
     if (file.fail())
     {
@@ -74,84 +34,33 @@ int main()
         return 1;
     }
 
-    int current_line = 0;
+    int current_line = 1;
     string line;
 
-    while (!file.eof())
+    do
     {
+        SteeringData data;
+
+        stringstream inputString(line);
         current_line++;
 
         getline(file, line);
 
-        cout << "Line: " << line << endl;
+        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
+
+        getline(inputString, data.Timestamp, ';');
+        getline(inputString, data.Acc_x, ';');
+        getline(inputString, data.Acc_y, ';');
+        getline(inputString, data.Acc_z, ';');
+
+        writeFile << data.Timestamp << " " << data.Acc_x << " " << data.Acc_y << " " << data.Acc_z << endl;
+
     }
+    while (!file.eof());
 
 
     file.close();
+    writeFile.close();
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-int main()
-{
-
-    string line;
-    ifstream ifs;
-
-    ifs.open("attitude_exam.log");
-
-    vector<SteeringData> steers;
-
-    while(getline(ifs, line))
-    {
-        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
-
-        stringstream inputString(line);
-
-        string timestamp;
-        string acc_x;
-        string acc_y;
-        string acc_z;
-
-        getline(inputString, timestamp, ';');
-        getline(inputString, acc_x, ';');
-        getline(inputString, acc_y, ';');
-        getline(inputString, acc_z, ';');
-
-        SteeringData steer(timestamp, acc_x, acc_y, acc_z);
-
-        steers.push_back(steer);
-
-    displayData(steers);
-    }
-
-}
-*/
